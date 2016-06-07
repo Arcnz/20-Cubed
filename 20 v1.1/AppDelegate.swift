c786a620d98c3842f16b5b30b8bc29099549d281
Arcnz <Arcnz@users.noreply.github.com>
2016-06-07 19:50:03 +0800

    Added source code

---
+++ 20 v1.1/AppDelegate.swift
@@ -0,0 +1,50 @@
+//
+//  AppDelegate.swift
+//  20 v1.1
+//
+//  Created by Austin on 26/05/2016.
+//  Copyright © 2016 Austin & Robin Co. Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    var timer       = NSTimer()
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+    
+    @IBAction func enableClicked(sender: NSMenuItem) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: "alert", userInfo: nil, repeats: true)
+        NSRunLoop.currentRunLoop().addTimer(timer, forMode: NSRunLoopCommonModes)
+    }
+    
+    func alert() {
+        let myPopup: NSAlert = NSAlert()
+        myPopup.messageText = "Friendly Reminder"
+        myPopup.informativeText = "Just a friendly reminder to ask you to please look away from the screen for 20 seconds."
+        myPopup.alertStyle = NSAlertStyle.WarningAlertStyle
+        myPopup.addButtonWithTitle("OK")
+        let res = myPopup.runModal()
+    }
+    
+    @IBAction func quitClicked(sender: NSMenuItem) {
+        NSApplication.sharedApplication().terminate(self)
+    }
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+        statusItem.title = "20³"
+        statusItem.menu = statusMenu
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
